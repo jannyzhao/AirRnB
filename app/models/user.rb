e5043d66d8f987d993f9a29aa7162e7bd706f01a
Janny Zhao <zhao.janny@gmail.com>
2023-02-24 16:54:25 -0800

    adding spireg

---
+++ app/models/user.rb
@@ -0,0 +1,65 @@
+class User < ApplicationRecord
+  # has_secure_password takes care of is_password? and password() from SPIREG
+  has_secure_password
+
+  # ensure that user has a session_token before doing validation
+  before_validation :ensure_session_token
+
+  validates :username, 
+    uniqueness: true,
+    length: { in: 3..30 },
+    format: { with: URI::MailTo::EMAIL_REGEXP, message: "cannot be an email" }
+
+  validates :email,
+    uniqueness: true,
+    length: { in: 3..255 }
+    format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  validates :password,
+    length: { in: 6..255 },
+    allow_nil: true
+
+  validates :session_token,
+    presence: true,
+    uniqueness: true
+
+  def self.find_by_credentials(credential, password)
+    if (URI::MailTo::EMAIL_REGEXP).match(credential)
+      user = User.find_by(email: credential)
+    else
+      user = User.find_by(username: credential)
+    end
+
+    # if user && user.authenticate(password)
+    #   return user
+    # else
+    #   return false
+    # end
+
+    user && user.authenticate(password) ? user : false
+  end
+
+  def reset_session_token
+    self.session_token = generate_unique_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+  def generate_unique_session_token
+    token = SecureRandom::urlsafe_base64
+    while User.exists?(session_token: token)
+        token = SecureRandom::urlsafe_base64
+    end
+    token
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end
+end
+
+
+
+
+
